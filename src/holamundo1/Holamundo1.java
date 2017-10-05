f501f28f915bb7f088b7157cf51c5a42d8f53326
ggarciaalvarez <ggarciaalvarez@victoria26.danielcastelao.org>
2017-10-05 09:17:17 +0200

    Hola Goku

--- src/holamundo1/Holamundo1.java
+++ src/holamundo1/Holamundo1.java
@@ -16,7 +16,10 @@ public class Holamundo1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
- 
+        
+        
+        
+        System.out.println("Hola Goku");
     }
     
 }
